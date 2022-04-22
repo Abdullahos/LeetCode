7da2f05d9a423886c1c0d53b90c19d46d2c43640
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-04-22 16:59:17 +0200

    Time: 54 ms (8.74%), Space: 44 MB (76.55%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -2,15 +2,20 @@ class Solution {
     public void moveZeroes(int[] nums) {
         int i = 0;
         int counts = 0;
-        while(i<nums.length-counts){
+        //parse over the array
+        while(i<nums.length-counts){    //O(N-N0) 
             if(nums[i]==0){
                 counts++;
-                for(int j = i; j<nums.length-counts; j++){
+                //shifting
+                for(int j = i; j<nums.length-counts; j++){  //O(N-N0)
                     nums[j] = nums[j+1];
                 }
+                //setting the duplicated item due to shifting to 0
                 nums[nums.length-counts] = 0;
             }
             else    i++;
         }
     }
 }
+//space complexity O(1)
+//time complexity O((N_N0)^2)
