577d4b42c1eab1ba224f8ae85e148cbc81fb17b8
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-04-26 02:17:26 +0200

    Time: 330 ms (14.59%), Space: 50.2 MB (13.39%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        final int size = 26;
+        //the following solution may fails for duplication letters in s2
+        /*if(s2.length()<s1.length())   return false;
+        int i = 0 ;
+        int minIdx = s2.length()-1;
+        int maxIdx = 0;
+        while(i<s1.length()) {
+            int idx = s2.indexOf(s1.charAt(i));
+            if(idx == -1)   return false;
+            if(idx<minIdx)  minIdx = idx;
+            if(idx>maxIdx)  maxIdx = idx;
+            i++;
+        }  
+        System.out.println(minIdx+" "+maxIdx);
+        return (maxIdx-minIdx+1)==s1.length()? true:false;
+    */
+        if(s2.length()<s1.length())   return false;
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+        
+        int[] freqs_1 = new int[size];
+        
+        for(char cc1: c1){
+            freqs_1[cc1-'a']++;
+        }
+        
+        for(int i = 0 ; i<s2.length()-s1.length()+1; i++){
+            int [] freqs_2 = new int[size];
+            for(int j = 0; j<s1.length(); j++){
+                freqs_2[s2.charAt(i+j)-'a']++;
+            }
+            boolean ever = true;
+            for(int j = 0; j<size; j++){
+                if(freqs_1[j]!=freqs_2[j])  {
+                    ever = false;
+                    break;
+                }
+            }
+            if(ever)    return true;
+        }
+        return false;
+    }
+}
