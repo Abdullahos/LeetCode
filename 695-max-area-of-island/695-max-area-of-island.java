03947d48d50eda0bc33630299a9ff13d6318bdaa
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-04-27 07:31:00 +0200

    Time: 5 ms (28.50%), Space: 42.6 MB (78.13%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -25,13 +25,4 @@ class Solution {
         }
         return max;
     }
-    public void print_array(int[][] grid){
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                System.out.print(grid[i][j]+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-    }
 }
