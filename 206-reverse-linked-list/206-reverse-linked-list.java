cb0e49037a6a0f4418ad31b6bc5c9417ffd31668
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-04-29 09:53:29 +0200

    Time: 0 ms (100.00%), Space: 43.1 MB (41.36%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.java
+++ 206-reverse-linked-list/206-reverse-linked-list.java
@@ -9,19 +9,6 @@
  * }
  */
 class Solution {
-    /*public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null)    return head;
-        ListNode n = null;
-        ListNode p  = null;
-        while(head != null){
-            n = head.next;
-            head.next = p;
-            p = head;
-            head = n;   
-        }
-        return p;
-    }
-    */
     public ListNode reverseList(ListNode head){
         if(head==null || head.next == null) return head;
         ListNode current = head;
