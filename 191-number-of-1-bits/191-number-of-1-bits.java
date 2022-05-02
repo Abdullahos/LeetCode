237710e107b989a8d9a766e6147c405234bacc9d
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-05-02 10:37:24 +0200

    Time: 0 ms (100.00%), Space: 39.2 MB (93.13%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.java
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -1,4 +1,5 @@
 public class Solution {
+    /*
     public int hammingWeight(int n) {
         int k = 0;
         String sn = Integer.toBinaryString(n);
@@ -7,4 +8,16 @@ public class Solution {
         }
         return k;
     }
+    */
+    public int hammingWeight(int n) {
+        int k = 0;
+        int mask = 1;
+        
+        for(int i = 0; i<32; i++){
+            if((n & mask) != 0)   k++;
+            mask <<= 1;
+        }
+        return k;
+    }
+
 }
