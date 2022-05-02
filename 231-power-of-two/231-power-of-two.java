1b76bc17beabd39197efda01c22646c39e2ad2f1
Abdullah Osama <51336081+Abdullahos@users.noreply.github.com>
2022-05-02 09:37:09 +0200

    Time: 55 ms (10.44%), Space: 43.7 MB (7.63%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
        double res = new Double(String.format("%.10f",Math.log(n)/Math.log(2)));
-    System.out.println(res);
        return res%1==0;
     }
 }
