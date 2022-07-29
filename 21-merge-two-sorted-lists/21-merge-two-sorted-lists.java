ca09813ecd8123c575cf4940a612aae88fa99f53
Abdullah Osama <abdullahelagami55@gmail.com>
2022-07-29 14:28:07 +0200

    Time: 0 ms (100.00%), Space: 41.8 MB (92.37%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -32,7 +32,8 @@ class Solution {
         return l1.val<=l2.val? l1: l2;
     }
     */
-        public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    /*
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
             if(l1 == null)  return l2;
             if(l2 == null)  return l1;
             ListNode head;
@@ -62,6 +63,42 @@ class Solution {
             else    current.next = l1;
             return head;
         }
+    */
+      public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            if(l1 == null) return l2;
+            if(l2 ==null ) return l1;
+            ListNode head;
+            if(l1.val < l2.val){
+                head = l1;
+                l1 = l1.next;
+            }
+            else{
+                head = l2;
+                l2 = l2.next;
+                
+            }
+            ListNode cur = head;
+            while(l1!=null && l2!=null){
+                if(l1.val < l2.val){
+                cur.next = l1;
+                l1 = l1.next;
+                }
+                else{
+                    cur.next = l2;
+                    l2 = l2.next;
+
+                }
+                cur = cur.next;
+            }
+            if(l1 !=null){
+                cur.next = l1;
+            }
+            else{
+                cur.next = l2;
+            }
+          return head;
+      }
+     
     
 }
 //time complexity O(N)
